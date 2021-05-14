38c83a078a45286c765e26a69d6953e0b54ac0a4
Swmnx <No>
2021-05-14 19:26:20 +0300

    Initial Commit

---
+++ MvpExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MvpExample
+//
+//  Created by swamnx on 14.05.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
